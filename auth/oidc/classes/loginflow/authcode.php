fb56372f4ff9e7f7c6dc7977b4cdafa2ae42560e
Andreas Skorczyk <me@andreas-sk.de>
2023-08-03 08:16:58 +0200

    CUSTOM: Use person_ein if available for claim
    This change enables the OIDC to determine a Moodle username based on the employeeNumber (aka EIN) and the source.
    If the EIN exists in the token, we create a deterministic username concatenating the source and the ein of the person, not the account, using the form <source>_<ein>.
    
    ref #2208332 #2754378 #2685838

--- auth/oidc/classes/loginflow/authcode.php
+++ auth/oidc/classes/loginflow/authcode.php
@@ -564,7 +564,13 @@ class authcode extends base {
 
         $usernamechanged = false;
         if ($oidcusername && $tokenrec && $oidcusername !== $tokenrec->oidcusername) {
-            $usernamechanged = true;
+            // PATCH - refs #2685838
+            // The tokenrec->oidcusername will never match the idtoken->upn,
+            // so never try to update it (leave $usernamechanged == false).
+            if (getenv('O365_ENABLE_EIN_PATCH') !== false) {
+            } else {
+                $usernamechanged = true;
+            }
         }
 
         $existingmatching = null;
@@ -731,9 +737,31 @@ class authcode extends base {
                     $username = $idtoken->claim('email');
                 }
             } else {
-                $username = $idtoken->claim('upn');
-                if (empty($username)) {
-                    $username = $idtoken->claim('unique_name');
+                // PATCH - refs #2754378
+                // If the token contains a claim for 'employeeNumber' (EIN), then
+                //   add the 'source' and create a reliable username
+                // else
+                //   let the original behavior suffice
+                if (getenv('O365_ENABLE_EIN_PATCH') !== false) {
+                    $username = $idtoken->claim('person_ein');
+                    if (!empty($username)) {
+                        $source = $idtoken->claim('person_source');
+                        if (empty($source)) {
+                            $source = 'unknown';
+                        }
+                        $username = $source . '_' . $username;
+                    }
+                    if (empty($username)) {
+                        $username = $idtoken->claim('upn');
+                    }
+                    if (empty($username)) {
+                        $username = $idtoken->claim('unique_name');
+                    }
+                } else {
+                    $username = $idtoken->claim('upn');
+                    if (empty($username)) {
+                        $username = $idtoken->claim('unique_name');
+                    }
                 }
             }
             $originalupn = null;
