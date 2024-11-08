6164a78384105104d844b5c56eaa0c63a6365231
Andreas Skorczyk <me@andreas-sk.de>
2024-11-08 11:43:57 +0100

    CUSTOM: Use person_ein if available for claim
    This change enables the OIDC to determine a Moodle username based on the employeeNumber (aka EIN) and the source.
    If the EIN exists in the token, we create a deterministic username concatenating the source and the ein of the person, not the account, using the form <source>_<ein>.
    
    ref #2208332 #2754378 #2685838 #3151861

--- auth/oidc/classes/loginflow/authcode.php
+++ auth/oidc/classes/loginflow/authcode.php
@@ -572,9 +572,17 @@ class authcode extends base {
         if (auth_oidc_is_local_365_installed()) {
             if ($existingmatching = $DB->get_record('local_o365_objects', ['type' => 'user', 'objectid' => $oidcuniqid])) {
                 $existinguser = core_user::get_user($existingmatching->moodleid);
-                if ($existinguser && strtolower($existingmatching->o365name) != strtolower($oidcusername) &&
-                    $existinguser->username != strtolower($oidcusername)) {
-                    $usernamechanged = true;
+                if (
+                    $existinguser && strtolower($existingmatching->o365name) != strtolower($oidcusername) &&
+                    $existinguser->username != strtolower($oidcusername)
+                ) {
+                    // PATCH - refs #2685838
+                    // The tokenrec->oidcusername will never match the idtoken->upn,
+                    // so never try to update it (leave $usernamechanged == false).
+                    if (getenv('O365_ENABLE_EIN_PATCH') !== false) {
+                    } else {
+                        $usernamechanged = true;
+                    }
                 }
             }
         }
